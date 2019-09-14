ffba4c91daf5c8be99f1580f581559878fc518dd
dannydaley <54544735+dannydaley@users.noreply.github.com>
2019-09-14 20:37:28 +0100

    update

--- script.js
+++ script.js
@@ -13,9 +13,13 @@ alert(total);
 alert("Starting log in sequence")
 */
 //FORM START
-var submit = document.getElementById("submit");
+/*var submit = document.getElementById("submit");
 var uname = document.getElementById("uname").value;
 var psw = document.getElementById("psw").value;
+*/
+
+var stylechange = document.getElementById("stylechange");
+var main = document.getElementById("main")
 
 function check()
 {
@@ -28,7 +32,7 @@ function check()
 	if (document.getElementById("uname").value === database[0].username &&
 		 document.getElementById("psw").value === database[0].password )  {
 			alert("All checks out, Nace..") ;} else {alert("Somethings not right.."); }
-*/
+
 
 submit.addEventListener("click", function(){  
 	check();
@@ -37,7 +41,20 @@ submit.addEventListener("click", function(){
 
 
 
+;*/
+
+stylechange.addEventListener("click", function() { changeBackground(); });
+
+function changeBackground(color) {
+   if (document.body.style.background === "black") {document.body.style.background = "white"; document.getElementById("main").style.color = "black";}
+   	else {document.body.style.background = "black"; document.getElementById("main").style.color = "white"};
+
+};
+
+
+/*
 ;
+
 /*
 checkByKeyPress.addEventListener("keypress", checkByKeyPress);
 checkByKeyPress();}
@@ -45,9 +62,9 @@ checkByKeyPress();}
 //FORM END
 
 
-//DATABASE
-var database = [{
-	username: "daley112",
-	password: "password123",}];
+//DATABASE()/*
+//var database = [{
+//	username: "daley112",
+//	password: "password123",}];
 //DATABASE END
-
+;
