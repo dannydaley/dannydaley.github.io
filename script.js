bd51aa369f0dac7c2a8d6fecee440619b790f39c
dannydaley <54544735+dannydaley@users.noreply.github.com>
2019-10-05 17:31:32 +0100

    Update
    home button added;
    about me page content updated;
    blog page content updated;
    animations added to content for better user experience;

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-/*var firstNumber = prompt("First Number Please");
-var secondNumber = prompt("Second Number Please");
-
-var total = (Number(firstNumber) + Number(secondNumber));
-alert(total);
-
-	{if (total <= 20) {alert("passed"); }
-
- else{alert("Something a little smaller?");
-}
-}
-
-alert("Starting log in sequence")
-*/
-//FORM START
-/*var submit = document.getElementById("submit");
-var uname = document.getElementById("uname").value;
-var psw = document.getElementById("psw").value;
-*/
 
 var stylechange = document.getElementById("stylechange");
 var main = document.getElementById("main")
@@ -28,20 +9,6 @@ function check()
 			alert("All checks out, Nace..");} else {alert("Somethings not right..");}
 
 }
-/*{
-	if (document.getElementById("uname").value === database[0].username &&
-		 document.getElementById("psw").value === database[0].password )  {
-			alert("All checks out, Nace..") ;} else {alert("Somethings not right.."); }
-
-
-submit.addEventListener("click", function(){  
-	check();
-}
-)
-
-
-
-;*/
 
 stylechange.addEventListener("click", function() { changeBackground(); });
 
@@ -50,21 +17,3 @@ function changeBackground(color) {
    	else {document.body.style.background = "black"; document.getElementById("main").style.color = "white"};
 
 };
-
-
-/*
-;
-
-/*
-checkByKeyPress.addEventListener("keypress", checkByKeyPress);
-checkByKeyPress();}
-*/
-//FORM END
-
-
-//DATABASE()/*
-//var database = [{
-//	username: "daley112",
-//	password: "password123",}];
-//DATABASE END
-;
